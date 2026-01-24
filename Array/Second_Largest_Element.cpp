3c9e52946fe8284dfdfcefbf973165347d9c08a2
Amit Kumar Verma <amitku86190@gmail.com>
2026-01-24 13:44:56 +0530

    Second_largest changed adding new approch

--- Array/Second_Largest_Element.cpp
+++ Array/Second_Largest_Element.cpp
@@ -1,17 +1,45 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int main(){
-    vector<int> nums={10 ,9,10};
-    int first=10;
-    int second=INT_MIN;
+// int main(){
+//     vector<int> nums={10 ,9,10};
+//     int first=10;
+//     int second=INT_MIN;
 
-    for(int i=0; i<nums.size(); i++){
-        if(nums[i]>=second && nums[i]<first){
-            second=nums[i];
-        }
-    }
-    cout<<first<<"   "<<second;
-    return 0; 
-}
+//     for(int i=0; i<nums.size(); i++){
+//         if(nums[i]>=second && nums[i]<first){
+//             second=nums[i];
+//         }
+//     }
+//     cout<<first<<"   "<<second;
+//     return 0; 
+// }
+
+
+// new method is added
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     vector<int> nums = {2,3,5,2,1,9,6,4,10};
+
+//     int first = INT_MIN;
+//     int second = INT_MIN;
+
+//     for(int i = 0; i < nums.size(); i++){
+//         if(nums[i] > first){
+//             second = first;
+//             first = nums[i];
+//         }
+//         else if(nums[i] < first && nums[i] > second){
+//             second = nums[i];
+//         }
+//     }
+
+//     cout << first << " " << second;
+//     return 0;
+// }
